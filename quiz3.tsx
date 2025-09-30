5c6e8bbd6e11d5be1447774fcac1f0ce4dfd7818
Jv650 <jemmyvictor1@gmail.com>
2025-09-29 21:42:08 -0700

    commit quiz

--- quiz3.tsx
+++ quiz3.tsx
@@ -19,3 +19,42 @@ return (
 )
 }
 export default ChildComponent;
+
+
+//////////////// correct way 
+import React, { useState } from 'react';
+
+// Parent
+const ParentComponent: React.FC = () => {
+  const [message, setMessage] = useState<string>('');
+
+  return (
+    <div>
+      <h2>Parent message: {message}</h2>
+      <ChildComponent message={message} setMessage={setMessage} />
+    </div>
+  );
+};
+
+export default ParentComponent;
+
+// Child
+type ChildProps = {
+  message: string;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const ChildComponent: React.FC<ChildProps> = ({ message, setMessage }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Type here"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default ChildComponent;
